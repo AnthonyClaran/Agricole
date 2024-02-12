6a499e0d8d4987e262a85f03ca8295dbc63a955f
fenoantra <106917754+fenoantra@users.noreply.github.com>
2024-02-12 12:47:45 +0100

    Create index.php

---
+++ p/index.php
@@ -0,0 +1,18 @@
+<<<<<<< HEAD
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/png" href="logo.png">
+    <title>Agricole</title>
+</head>
+<body>
+    
+</body>
+</html>
+=======
+<?php
+    header('location:index.php');
+?>
+>>>>>>> df42ebed3166dc796d981e52e125d6ac8870debd
